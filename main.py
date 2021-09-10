a8f98a3fc8d2041f718d9187b9a6d32e90b76dac
aa2082 <aa2082@cam.ac.uk>
2021-09-10 16:18:16 +0100

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw
 import numpy as np
 from cell import make_cell
 
-img_dim = 320 #pixel dimensions of image
+img_dim = 3200 #pixel dimensions of image, more the longer to render
 hwa_lab_xy_max = 40
 multiplication_factor = img_dim/(2*hwa_lab_xy_max)
 hwa_lab_r = 0.5
@@ -14,7 +14,7 @@ bw_data = np.zeros((img_dim,img_dim), dtype=np.uint8)
 #L indicates mode: (8-bit pixels, black and white)
 #https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
 
-myFile = np.genfromtxt('30.csv', delimiter=' ')
+myFile = np.genfromtxt('80.csv', delimiter=' ')
 cells = myFile[:,[3,4,6,7]]
 cells = (cells*multiplication_factor)+img_dim/2
 num_cells = np.shape(cells)[0]
